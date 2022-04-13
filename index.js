f7f97125b129dcccc036c7a9f11e2e19044493e4
brockpaperscissors <michaeljbrock91@gmail.com>
2022-04-12 20:02:22 -0700

    gave app controllers to use

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const questionController = require('./controllers/Questions');
+app.use('/questions', questionController);
+
+const sessionController = require('./controllers/Session');
+app.use('/sessions', sessionController);
+
 app.listen(4000, () => {
 	console.log('app listening on port 4000');
 });
