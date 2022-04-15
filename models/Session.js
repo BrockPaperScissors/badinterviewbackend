520008436e4b930f1f0bbe408dccfd12758f2d92
brockpaperscissors <michaeljbrock91@gmail.com>
2022-04-15 13:02:37 -0700

    updated session model

--- models/Session.js
+++ models/Session.js
@@ -2,6 +2,7 @@ const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
 const SessionSchema = new Schema({
+	sessionId: { type: Number, require: true },
 	sessionName: { type: String, require: true },
 	numQuestions: { type: Number, require: true },
 	questions: [String],
